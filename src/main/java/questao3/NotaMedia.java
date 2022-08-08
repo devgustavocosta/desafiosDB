c9ee43632acb45e6ea41097d3a5eae0544997b30
devgustavocosta <gustavocs8@outlook.com>
2022-08-08 10:27:44 -0300

    primeiro commit

---
+++ src/main/java/questao3/NotaMedia.java
@@ -0,0 +1,30 @@
+package questao3;
+
+import java.util.Scanner;
+
+public class NotaMedia {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = entrada.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = entrada.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = entrada.nextDouble();
+
+        double calculo = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("---------------------------------");
+        System.out.println("Sua nota foi: " + calculo);
+
+        System.out.println("---------------------------------");
+        if (calculo < 7){
+            System.out.println("Aluno reprovado");
+        } else {
+            System.out.println("Aluno aprovado");
+        }
+    }
+}
